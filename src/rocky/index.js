2f0052fb26687cf50108edfc0967aeb57c85d66b
Alexander Koziol <alekoz47@gmail.com>
2017-01-04 12:08:50 -0500

    bug fixes

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -1,42 +1,57 @@
 var rocky = require("rocky");
 
-function draw(event) {
-	var ctx = event.context;
+function draw(ctx) {
 	var d = new Date();
 	var w = ctx.canvas.unobstructedWidth;
 	var h = ctx.canvas.unobstructedHeight;
-	var days = d.getDate();
+	var days = d.getDay();
 	var hours = d.getHours();
 	var minutes = d.getMinutes();
 	
 	ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-	drawBackground(ctx, days, hours, minutes, [w, h]);
-	drawTime(ctx, days, hours, minutes, [w, h]);
+	drawBackground(ctx, days, hours, minutes, w, h);
+	drawTime(ctx, days, hours, minutes, w, h);
 }
 
-function drawBackground(ctx, d, h , m, dim) {
+function drawBackground(ctx, d, h , m, x, y) {
 	var colorString = dateToHex(d, h, m);
-	var color = colorString.parseInt(16);
+	var color = colorString;
 	ctx.fillStyle = color;
-	ctx.fillRect(0, 0, dim[0], dim[1]);
+	ctx.fillRect(0, 0, x, y);
 }
 
-function drawTime(ctx, d, h, m, dim) {
+function drawTime(ctx, d, h, m, x, y) {
 	ctx.textAlign = "center";
 	ctx.font = "28px bold Droid-serif";
 	ctx.fillStyle = "white";
-	ctx.fillText(h.toString + "" + m.toString, dim[0] / 2, dim[1] / 2);
+	var textAdjust = ctx.measureText("0").height / 2;
+	ctx.fillText(h.toString() + "h" + m.toString(), x / 2, (y / 2) - textAdjust);
 }
 
-function dateToHex(lod) {
-	var acc = "#";
-	for (var i = 0; i < lod.length; i++) {
-		acc = acc + lod[i].toString(16);
-	}
+function dateToHex(d, h, m) {
+	// TODO: Remove console.log() statements
+	console.log(d);
+	console.log(h);
+	console.log(m);
+	d = Math.round(convertRange(d, 0, 31, 0, 255));
+	h = Math.round(convertRange(h, 0, 60, 0, 255));
+	m = Math.round(convertRange(m, 0, 60, 0, 255));
+	console.log(d);
+	console.log(h);
+	console.log(m);
+	var dateHexString = "#" + d.toString(16) + h.toString(16) + m.toString(16);
+	console.log(dateHexString);
+	return dateHexString;
+}
+
+function convertRange(x, xMin, xMax, yMin, yMax) {
+	// TODO: Fix this, maybe switched
+	var percent = (x - xMin) / (xMax - xMin);
+	return percent * (yMax - yMin) + yMin;
 }
 
 rocky.on("draw", function(event) {
-	draw(event);
+	draw(event.context);
 });
 
 rocky.on("minutechange", function(event) {
