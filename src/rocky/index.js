93aa2f3dd8951ef3018fec7b8b5f240dce8524d1
Alexander Koziol <alekoz47@gmail.com>
2017-01-06 00:19:33 -0500

    add date to color

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -4,21 +4,22 @@ function draw(ctx) {
 	var d = new Date();
 	var w = ctx.canvas.unobstructedWidth;
 	var h = ctx.canvas.unobstructedHeight;
+	var days = d.getDay();
 	var hours = d.getHours();
 	var minutes = d.getMinutes();
-	var color = dateToHex(hours, minutes);
+	var color = dateToHex(days, hours, minutes);
 	
 	ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-	drawBackground(ctx, color, hours, minutes, w, h);
-	drawTime(ctx, color, hours, minutes, w, h);
+	drawBackground(ctx, color, days, hours, minutes, w, h);
+	drawTime(ctx, color, days, hours, minutes, w, h);
 }
 
-function drawBackground(ctx, color, h, m, x, y) {
+function drawBackground(ctx, color, d, h, m, x, y) {
 	ctx.fillStyle = color;
 	ctx.fillRect(0, 0, x, y);
 }
 
-function drawTime(ctx, color, h, m, x, y) {
+function drawTime(ctx, color, d, h, m, x, y) {
 	var hex = "0x";
 	var colorList = color.split("");
 	var red = hex.concat(colorList.slice(1, 3));
@@ -29,9 +30,9 @@ function drawTime(ctx, color, h, m, x, y) {
 	blue = parseInt(blue, 10);
 	
 	if ((0.299 * red + 0.587 * green + 0.114 * blue) > 127) {
-		ctx.fillStyle = "white";
-	} else {
 		ctx.fillStyle = "black";
+	} else {
+		ctx.fillStyle = "white";
 	}
 	ctx.textAlign = "center";
 	ctx.font = "42px bold numbers Leco-numbers";
@@ -44,15 +45,17 @@ function drawTime(ctx, color, h, m, x, y) {
 	ctx.fillText(m, x / 2, (y / 2) - 13 + (textAdjust * 0.25));
 }
 
-function dateToHex(h, m) {
-	h = Math.round(convertRange(h, 0, 24, 0, 4095));
-	m = Math.round(convertRange(m, 0, 60, 0, 4095));
-	console.log(h + " : " + m);
+function dateToHex(d, h, m) {
+	d = Math.round(convertRange(d, 0, 31, 0, 255));
+	h = Math.round(convertRange(h, 0, 24, 0, 255));
+	m = Math.round(convertRange(m, 0, 60, 0, 255));
+	console.log(d + " : " + h + " : " + m);
 	
-	h = extendString(h.toString(16), 3);
-	m = extendString(m.toString(16), 3);
+	d = extendString(d.toString(16), 2);
+	h = extendString(h.toString(16), 2);
+	m = extendString(m.toString(16), 2);
 	
-	var dateHexString = "#" + h.toString(16) + m.toString(16);
+	var dateHexString = "#" + d.toString(16) + h.toString(16) + m.toString(16);
 	console.log(dateHexString);
 	return dateHexString;
 }
@@ -64,7 +67,7 @@ function convertRange(x, xMin, xMax, yMin, yMax) {
 
 function extendString(string, length) {
 	if (string.length < length) {
-		return "0".concat(string);
+		return extendString("0".concat(string), length);
 	} else {
 		return string;
 	}
