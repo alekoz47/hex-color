bbb6c0be2daee5889e8e169b9897e60fa089c51a
Alexander Koziol <alekoz47@gmail.com>
2017-01-07 23:46:29 -0500

    refactor code

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -22,33 +22,23 @@ function drawBackground(ctx, color, d, h, m, x, y) {
 }
 
 function drawTime(ctx, color, d, h, m, x, y) {
-	color = parseInt(color, 16);
-	var red = (color & 0xFF0000) >> 16;
-	var green = (color & 0xFF00) >> 8;
-	var blue = color & 0xFF;
-	console.log("split color: " + red + " : " + green + " : " + blue);
+	color = hexStringToColor(color);
 	
-	if ((0.299 * red + 0.587 * green + 0.114 * blue) > 127) {
-		ctx.fillStyle = "black";
-	} else {
-		ctx.fillStyle = "white";
-	}
+	setTextColor(ctx, color);
 	ctx.textAlign = "center";
 	ctx.font = "42px bold numbers Leco-numbers";
 	
 	h = extendString(h.toString(), 2);
 	m = extendString(m.toString(), 2);
-	
 	var textAdjust = ctx.measureText("0").height;
 	ctx.fillText(h, x / 2, (y / 2) - (textAdjust * 1.25));
 	ctx.fillText(m, x / 2, (y / 2) - 13 + (textAdjust * 0.25));
 	
 	ctx.font = "18px Gothic";
-	red = extendString(red.toString(16), 2);
-	green = extendString(green.toString(16), 2);
-	blue = extendString(blue.toString(16), 2);
-	var colorString = red + green + blue;
-	
+	color.red = extendString(color.red.toString(16), 2);
+	color.green = extendString(color.green.toString(16), 2);
+	color.blue = extendString(color.blue.toString(16), 2);
+	var colorString = color.red + color.green + color.blue;
 	d = extendString(d.toString(), 2);
 	var dateHeightAdjust = (y / 2) - ctx.measureText(d).height;
 	
@@ -76,6 +66,34 @@ function dateToHex(d, h, m) {
 	var dateHexString = d.toString(16) + h.toString(16) + m.toString(16);
 	console.log("hex color: " + dateHexString);
 	return dateHexString;
+}
+
+function hexStringToColor(hex) {
+	hex = parseInt(hex, 16);
+	var red = (hex & 0xFF0000) >> 16;
+	var green = (hex & 0xFF00) >> 8;
+	var blue = hex & 0xFF;
+	console.log("split color: " + red + " : " + green + " : " + blue);
+	
+	var color = {
+		red: red,
+		green: green,
+		blue: blue,
+	};
+	
+	return color;
+}
+
+function setTextColor(ctx, color) {
+	var redMap = color.red * 0.299;
+	var greenMap = color.green * 0.587;
+	var blueMap = color.blue * 0.114;
+	
+	if ((redMap + greenMap + blueMap) > 127.5) {
+		ctx.fillStyle = "black";
+	} else {
+		ctx.fillStyle = "white";
+	}
 }
 
 function convertRange(x, xMin, xMax, yMin, yMax) {
